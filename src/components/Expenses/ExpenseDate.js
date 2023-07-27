0faad49ac8ea8d1e4be159c0d5eed52d3a16d9eb
SAEMMM <saem030@naver.com>
2023-07-27 21:39:16 +0900

    [Feat] state props까지

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ExpenseDate.css'
+
+function ExpenseDate(props) {
+
+    // date에서 '월'만 문자로 빼냄
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+    const year = props.date.getFullYear()
+
+    return (
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__year'>{year}</div>
+            <div className='expense-date__day'>{day}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate
